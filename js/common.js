ef8ce2a21936bd450f90a57a2cbc3537474df2ac
jerryachel <jerryachel@foxmail.com>
2017-12-08 16:55:40 +0800

    修改接口域名

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
+var baseUrl = 'http://back.n2o-x.cn';
 (function(){
-	$.get('http://back.n2o.gzbige.com/n2oPublic/companyInfo.do',function(res){
+	$.get(baseUrl+'/n2oPublic/companyInfo.do',function(res){
             var data = res.model
             var html = template('companyInfo',data);  
             document.getElementById("footer_info").innerHTML = html; 
