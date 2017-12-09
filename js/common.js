40fb8f8df5c98210b95499f674025a597579a41e
jerryachel <jerryachel@foxmail.com>
2017-12-09 20:18:43 +0800

    更换首页样式

--- js/common.js
+++ js/common.js
@@ -1,8 +1,9 @@
 var baseUrl = 'http://back.n2o-x.cn';
 (function(){
 	$.get(baseUrl+'/n2oPublic/companyInfo.do',function(res){
-            var data = res.model
+            /*var data = res.model
             var html = template('companyInfo',data);  
-            document.getElementById("footer_info").innerHTML = html; 
+            document.getElementById("footer_info").innerHTML = html; */
+            $(".tel_phone").text(res.model.telePhone)
         })
 })()
