bb1562a4d837942096c639fc53ccf96d9223f07d
jerryachel <jerryachel@foxmail.com>
2017-12-13 21:51:54 +0800

    修改顶部底部

--- js/common.js
+++ js/common.js
@@ -1,9 +1,11 @@
 var baseUrl = 'http://back.n2o-x.cn';
+var tel_phone = '';
 (function(){
 	$.get(baseUrl+'/n2oPublic/companyInfo.do',function(res){
             /*var data = res.model
             var html = template('companyInfo',data);  
             document.getElementById("footer_info").innerHTML = html; */
             $(".tel_phone").text(res.model.telePhone)
+            tel_phone = res.model.telePhone
         })
 })()
